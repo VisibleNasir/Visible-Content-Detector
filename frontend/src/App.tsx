16b734f6cb2199dc9e17e0a19e6f33eb11868fc9
Nasir Nadaf <109416738+VisibleNasir@users.noreply.github.com>
2025-04-11 14:19:37 +0530

    Created frontend files

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-black'>
+        <p>Hello </p>
+      </div>
+    </>
+  )
+}
+
+export default App
